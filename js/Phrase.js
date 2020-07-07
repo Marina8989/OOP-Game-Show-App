5f222c5218f1b696cd67409947dfdcfd9cd96162
Marina8989 <marina.mill209@gmail.com>
2020-07-06 20:11:39 -0700

    added js in Phrase file

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,25 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase{
+    constructor(phrase) {
+       this.phrase = phrase.toLowerCase();
+    }
+    addPhraseToDisplay() {
+        const ul = document.querySelectoer('#phrase ul');
+        //use .split() in order to be able to loop through each character in the string
+        const phrase = this.phrase.split('');
+        phrase.forEach(letter => {
+            const li = document.createElement('li');
+            li.textContent = letter;
+            if(letter === '') {
+               li.className = 'space';
+            }else {
+                li.className = `hide letter ${letter}`;
+            }
+          ul.appendChild(li);
+        });
+    }
+    checkLetter() {
+
+    }
+    showMatchedLetter(){}
+}
+
