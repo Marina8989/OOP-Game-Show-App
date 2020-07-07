897bdb0756ed45b008caeaeb73bef66eb6c2510d
Marina8989 <marina.mill209@gmail.com>
2020-07-06 20:26:11 -0700

    finished with one part

--- js/Phrase.js
+++ js/Phrase.js
@@ -17,9 +17,33 @@ class Phrase{
           ul.appendChild(li);
         });
     }
-    checkLetter() {
-
+    checkLetter(letter) {
+       return this.phrase.includes(letter);
     }
-    showMatchedLetter(){}
+    showMatchedLetter(letter){
+     const liLetters = document.querySelectorAll('#phrase.letter');
+       liLetters.forEach(character => {
+           if(character.textContent === letter) {
+               character.classList.remove('hide');
+               character.classList.add('show');
+           }
+       });
+    }
 }
 
+
+// <div id="phrase" class="section">
+//   <ul>
+//     <li class="hide letter h">h</li>
+//     <li class="hide letter o">o</li>
+//     <li class="hide letter w">w</li>
+//     <li class="space"> </li>
+//     <li class="hide letter a">a</li>
+//     <li class="hide letter r">r</li>
+//     <li class="hide letter e">e</li>
+//     <li class="space"> </li>
+//     <li class="hide letter y">y</li>
+//     <li class="hide letter o">o</li>
+//     <li class="hide letter u">u</li>
+//   </ul>
+// </div>;
