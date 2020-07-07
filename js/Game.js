ba180fa0c632773ef0dec5763222661c16fa1ba9
Marina8989 <marina.mill209@gmail.com>
2020-07-06 20:36:20 -0700

    added methods

---
+++ js/Game.js
@@ -0,0 +1,14 @@
+class Game {
+    constructor() {
+      this.missed = 0;
+      this.phrases = this.createPhrase();
+      this.activePhrase = null;
+    }
+    createPhrases(){}
+    startGame() {}
+    getRandomPhrase() {}
+    handleInteraction(){}
+    removeLife() {}
+    checkForWin() {}
+    gameOver(){}
+}
